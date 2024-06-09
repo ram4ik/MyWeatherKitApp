4ef640c882462ea00ebb77c24afb993bf1283440
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-06-09 18:53:06 +0300

    init

---
+++ MyWeatherKitApp/AttributionView.swift
@@ -0,0 +1,41 @@
+//
+//  AttributionView.swift
+//  MyWeatherKitApp
+//
+//  Created by Ramill Ibragimov on 6/9/24.
+//
+
+import SwiftUI
+import WeatherKit
+
+struct AttributionView: View {
+    @Environment(\.colorScheme) private var colorScheme
+    let weatherManager = WeatherManager.shared
+    @State private var attribution: WeatherAttribution?
+    
+    var body: some View {
+        VStack {
+            if let attribution {
+                AsyncImage(
+                    url: colorScheme == .dark ? attribution.combinedMarkDarkURL : attribution.combinedMarkLightURL) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 20)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                Text(.init("[\(attribution.serviceName)](\(attribution.legalPageURL))"))
+            }
+        }
+        .task {
+            Task.detached { @MainActor in
+                attribution = await weatherManager.weatherAttribution()
+            }
+        }
+    }
+}
+
+#Preview {
+    AttributionView()
+}
--- MyWeatherKitApp/MyWeatherKitAppApp.swift
+++ MyWeatherKitApp/MyWeatherKitAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MyWeatherKitAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ForecastView()
         }
     }
 }
