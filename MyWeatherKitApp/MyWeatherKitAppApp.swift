9b31fdd96ce83b5465bfcfbc289540ab854defcc
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-06-09 17:39:15 +0300

    Initial Commit

---
+++ MyWeatherKitApp/MyWeatherKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherKitAppApp.swift
+//  MyWeatherKitApp
+//
+//  Created by Ramill Ibragimov on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
